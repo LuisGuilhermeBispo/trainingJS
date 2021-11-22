b543c47a9439c7b982cd709ccc49bb3325c2dc4d
Luis Bispo <luisguilhermebbispo@hotmail.com.br>
2021-11-22 16:58:44 -0300

    Aula 3

--- bytebank/index.js
+++ bytebank/index.js
@@ -1,28 +1,6 @@
-class Cliente{
-    nome;
-    cpf;
-}
+import Cliente from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
-class ContaCorrente{
-    agencia;
-     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
-        this._saldo += valor;           
-    }
-}
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -35,12 +13,6 @@ cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
-
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+contaCorrenteRicardo.cliente = cliente1;
 
 console.log(contaCorrenteRicardo);
