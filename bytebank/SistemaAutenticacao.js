b3cd1125255367c12da52bce264bab40a6bc9402
Luis Bispo <luisguilhermebbispo@hotmail.com>
2021-12-16 16:40:34 -0300

    Aula 4: Sistema Interno

---
+++ bytebank/SistemaAutenticacao.js
@@ -0,0 +1,9 @@
+/*
+Ser autenticavel significa ter um método "senha"
+*/
+
+export class SistemaAutenticacao{
+    static login(autenticavel, senha){
+        return autenticavel.autenticar(senha);
+    }
+}
